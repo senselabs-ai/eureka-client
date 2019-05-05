b525b27faa0261ac500ea966d82c0c1aa0d3e1fe
Anthony Meikle <anthony.meikle@xyleminc.com>
2019-05-04 20:10:34 -0400

    resolving issues

--- src/main/java/ai/senselabs/enterprise/eurekaclient/EurekaClientApplication.java
+++ src/main/java/ai/senselabs/enterprise/eurekaclient/EurekaClientApplication.java
@@ -1,10 +1,23 @@
 package ai.senselabs.enterprise.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Configuration
+@EnableEurekaClient
+@RestController
 @SpringBootApplication
 public class EurekaClientApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "This is a trivial service that demonstrates how a Eureka Client can register with a Eureka Server";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
